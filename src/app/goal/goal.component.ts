6e69931075e192f99ceca095077130e272b41eec
Preston-Too <toopreston@gmail.com>
2020-11-12 15:20:29 +0300

    made changes

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Goal } from '../goal';
+import {GoalService} from '../goal-service/goal.service'
+import { AlertService } from '../alert-service/alert.service';
+import { HttpClient } from '@angular/common/http';
+import { Quote } from '../quote-class/quote';
+import { QuoteRequestService } from '../quote-http/quote-request.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-goal',
@@ -7,14 +13,14 @@ import { Goal } from '../goal';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
-  goals: Goal[] = [
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
-    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
-  ];
+  goToUrl(id){
+    this.router.navigate(['/goals',id])
+  }
+
+  goals:Goal[]
+  alertService:AlertService;
+  quote:Quote;
+    
   addNewGoal(goal){
     let goalLength = this.goals.length;
     goal.id = goalLength+1;
@@ -22,23 +28,34 @@ export class GoalComponent implements OnInit {
     this.goals.push(goal)
   }
 
-  toggleDetails(index){
-    this.goals[index].showDescription = !this.goals[index].showDescription;
-  }
+  deleteGoal(index){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
 
-  deleteGoal(isComplete, index){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
-
-      if (toDelete){
-        this.goals.splice(index,1)
-      }
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("Goal has been deleted")
     }
   }
+  constructor(goalService:GoalService, alertService:AlertService, private http:HttpClient, private quoteService:QuoteRequestService,  private router:Router) { 
+    this.goals = goalService.getGoals()
+    this.alertService = alertService;
 
-  constructor() { }
+    this.quoteService.quoteRequest()
+    this.quote = this.quoteService.quote
+  }
 
   ngOnInit(): void {
+    interface ApiResponse{
+      author:string;
+      quote:string;
+    }
+    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data=>{
+      // Succesful API request
+      this.quote = new Quote(data.author, data.quote)
+    },err=>{
+      this.quote = new Quote("Winston Churchill","Never never give up!")
+      console.log("An error occurred")
+    })
   }
 
 }
